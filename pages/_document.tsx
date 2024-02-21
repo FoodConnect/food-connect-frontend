f6406f2350db02f68aea8d082e36b3e79a44eb0a
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-02-20 19:50:08 -0800

    Changes _document to class and uses initial props

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,16 +1,8 @@
-import { Html, Head, Main, NextScript } from 'next/document';
-import { ColorSchemeScript } from '@mantine/core';
+import Document from 'next/document';
+import { createGetInitialProps } from '@mantine/next';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        <ColorSchemeScript />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+const getInitialProps = createGetInitialProps();
+
+export default class _Document extends Document {
+  static getInitialProps = getInitialProps;
 }
