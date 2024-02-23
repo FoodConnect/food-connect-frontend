5ce582954eaf0dd4fff069a1f1ad14ce6c7cf922
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-02-22 17:07:17 -0800

    Refactors Authentication form in anticipation of OAuth backend capabilities

---
+++ components/ApplicationContainer.ts
@@ -0,0 +1,7 @@
+import { AppShell, Footer, Group, Header, Text } from '@mantine/core';
+
+export const ApplicationContainer = ({ children }) => {
+  return(
+    <AppShell><AppShell>
+  )
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { AuthenticationForm } from '@/components/AuthenticationForm/AuthenticationForm';
 
 export default function HomePage() {
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <AuthenticationForm />
     </>
   );
 }
