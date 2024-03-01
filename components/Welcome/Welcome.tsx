632866ddb4bea05bf5aed6c40e55a9e5870536f1
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-03-01 12:47:31 -0800

    Defines color costom pallete

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -1,4 +1,4 @@
-import { Title, Text, Anchor } from '@mantine/core';
+import { Title, Text, Anchor, Flex, Button } from '@mantine/core';
 import classes from './Welcome.module.css';
 
 export function Welcome() {
@@ -10,7 +10,7 @@ export function Welcome() {
           Mantine
         </Text>
       </Title>
-      <Text color="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
+      <Text c="dimmed" ta="center" size="lg" maw={580} mx="auto" mt="xl">
         This starter Next.js project includes a minimal setup for server side rendering, if you want
         to learn more on Mantine + Next.js integration follow{' '}
         <Anchor href="https://mantine.dev/guides/next/" size="lg">
@@ -18,6 +18,25 @@ export function Welcome() {
         </Anchor>
         . To get started edit index.tsx file.
       </Text>
+      <Flex
+        mih={50}
+        bg="theme.colors.myPalette.9"
+        gap="md"
+        justify="flex-start"
+        align="flex-start"
+        direction="row"
+        wrap="wrap"
+      >
+        <Button color="chartreuse">chartreuse</Button>
+        <Button color="shrek">shrek</Button>
+        <Button color="sage">sage</Button>
+        <Button color="green">green</Button>
+        <Button color="emerald">emerald</Button>
+        <Button color="teal">teal</Button>
+        <Button color="sky">sky</Button>
+        <Button color="blue">blue</Button>
+        <Button color="navy">navy</Button>
+      </Flex>
     </>
   );
 }
