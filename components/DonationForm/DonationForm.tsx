628afd6280d902f3c9c0c73da82a0a55a0cf2d76
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-03-04 14:55:36 -0800

    Adds checkbox, dropzone, and text field to Donation form

--- components/DonationForm/DonationForm.tsx
+++ components/DonationForm/DonationForm.tsx
@@ -1,26 +1,33 @@
-import { Grid, NumberInput, ScrollArea, Skeleton, Text, Textarea } from '@mantine/core';
+import { Grid, NumberInput, ScrollArea, Text, Textarea } from '@mantine/core';
 import { useState } from 'react';
 import { DateValue } from '@mantine/dates';
 import GradientSegmentedControl from '../Inputs/GradientSegmentControl/GradientSegmentedControl';
 import TooltipIcon from '../Inputs/InputTooltips/TooltipIcon';
 import GradientDatePicker from '../Inputs/GradientDatePicker/GradientDatePicker';
+import CheckboxCard from '../Inputs/CheckboxCard/CheckboxCard';
+import { DropzoneButton } from '../Inputs/DopzoneButton/DropzoneButton';
 
 export default function DonationForm() {
-  const tooltipText =
+  const donationTooltipText =
     'Write something that describes your donation concisely for charities to veiw.';
+  const imageUrlTooltipText =
+    'This field will eventually go away. It exists to save Image URLs until the above dropzone is functioning.';
   const categoryOptions = ['Produce', 'Canned', 'Dairy', 'Dry'];
-  const child = <Skeleton height={300} radius="md" animate={false} />;
   const [value, setValue] = useState<DateValue>(new Date());
   return (
     <Grid>
       <Grid.Col span={{ base: 12, xs: 4 }}>
-        <TooltipIcon tooltipText={tooltipText} label="Title" placeholder="Donation title..." />
+        <TooltipIcon
+          tooltipText={donationTooltipText}
+          label="Title"
+          placeholder="Donation title..."
+        />
         <Textarea
           label="Description"
           placeholder="Write something about your donation..."
           autosize
-          minRows={2}
-          maxRows={4}
+          minRows={6}
+          maxRows={6}
         />
         <NumberInput
           label="Quantity"
@@ -39,8 +46,19 @@ export default function DonationForm() {
           Pick-Up Deadline
         </Text>
         <GradientDatePicker value={value} setValue={setValue} />
+        <CheckboxCard
+          label="Currently Available"
+          description="You can change this at any point in your donation's settings"
+        />
       </Grid.Col>
-      <Grid.Col span={{ base: 12, xs: 4 }}>{child}</Grid.Col>
+      <Grid.Col span={{ base: 12, xs: 4 }}>
+        <DropzoneButton />
+        <TooltipIcon
+          tooltipText={imageUrlTooltipText}
+          label="Image URL"
+          placeholder="Image Address..."
+        />
+      </Grid.Col>
     </Grid>
   );
 }
