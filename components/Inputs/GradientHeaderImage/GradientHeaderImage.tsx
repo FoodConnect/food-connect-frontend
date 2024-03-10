0852e3e30bdf472f4982ae837e99536e3a131bf6
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-03-10 11:11:55 -0700

    Adds non-null operators to gradient header color accessors

--- components/Inputs/GradientHeaderImage/GradientHeaderImage.tsx
+++ components/Inputs/GradientHeaderImage/GradientHeaderImage.tsx
@@ -1,7 +1,7 @@
 import { Overlay, Image, Card } from '@mantine/core';
 import { theme } from '@/theme';
 
-console.log(theme?.colors?.navy[9]);
+console.log(theme!.colors!.navy![9]);
 
 const GradientHeaderImage = () => (
   <Card shadow="none" padding="lg" radius="md" withBorder>
@@ -9,7 +9,7 @@ const GradientHeaderImage = () => (
       <Image src="/market.png" height={400} alt="Produce market grayscale image" />
       <Overlay
         style={{ zIndex: 1, radius: '1rem' }}
-        gradient={`linear-gradient(145deg, ${theme.colors.navy[9]} 0%, ${theme.colors.chartreuse[0]} 100%)`}
+        gradient={`linear-gradient(145deg, ${theme!.colors!.navy![9]} 0%, ${theme!.colors!.chartreuse![0]} 100%)`}
         opacity={0.85}
       />
       {/* <Overlay style={{ zIndex: 1, radius: '1rem' }} color={theme.colors.blue[6]} opacity={0.85} /> */}
