20742a84db55baf0f8b6fe43383f883bd449407e
Leah Perri <perrileah2023@gmail.com>
2024-04-26 14:31:54 -0500

    user must be signed in to claim a donation

--- components/AddToCartComponent/AddToCartComponent.tsx
+++ components/AddToCartComponent/AddToCartComponent.tsx
@@ -1,14 +1,25 @@
 import { Text, Group, Paper, Button, NumberInput } from '@mantine/core';
 import { IconShoppingCart, IconDeviceAnalytics } from '@tabler/icons-react';
 import { useForm } from '@mantine/form';
+import { useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 export default function AddToCartComponent() {
+  const { data: session, status } = useSession()
   const form = useForm({
     initialValues: {
       donation_id: 3,
       quantity: 0,
     },
   });
+
+  if (!session) {
+    return (
+    <div>
+      <Link href="/signin">You must be signed in to add to a cart.</Link>
+    </div>
+    );
+  }
 
   return (
     <Paper withBorder p="md" radius="md">
