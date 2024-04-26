f4c219cf3e71148d333a750c2e87e3795ff41567
Leah Perri <perrileah2023@gmail.com>
2024-04-26 16:04:06 -0500

    uses session token in authorization headers, success on frontend but not hitting db

--- utils/createCartedDonation.ts
+++ utils/createCartedDonation.ts
@@ -1,11 +1,20 @@
+import { getSession } from 'next-auth/react';
 import { CartedDonationData } from '@/components/Interfaces/CartedDonationData';
 
 export async function createCartedDonation(values: CartedDonationData) {
   try {
+    const session = await getSession();
+    if (!session) {
+      throw new Error('User not authenticated');
+    }
+
+    const token = session.access_token;
+
     const response = await fetch('http://localhost:8080/carts/add_to_cart/', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(values),
     });
