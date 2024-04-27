9b89f125dc651578f387e6a08706c4ba241359a7
Leah Perri <perrileah2023@gmail.com>
2024-04-27 15:51:40 -0500

    adds checkout button to order summary component

--- components/OrderSummaryComponent/OrderSummaryComponent.tsx
+++ components/OrderSummaryComponent/OrderSummaryComponent.tsx
@@ -1,11 +1,12 @@
-import { Paper, Container} from '@mantine/core';
+import { Paper, Container, Button } from '@mantine/core';
 
 export default function OrderSummaryComponent() {
 return (
   <Container my="md">
     <Paper shadow="md" radius="xs" withBorder p="xl">
-      <h2>Order Summary</h2>
+      <h3>Order Summary</h3>
       <p>You are ordering X donations today from X local vendors.</p>
+      <Button>Checkout</Button>
     </Paper>
   </Container>
 );
