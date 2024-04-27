a5692368c5e03e7d365fb8bacd4a8532e966855b
Leah Perri <perrileah2023@gmail.com>
2024-04-27 13:52:06 -0500

    combines logic of add to cart into one component

--- components/AddToCartComponent/AddToCartComponent.tsx
+++ components/AddToCartComponent/AddToCartComponent.tsx
@@ -5,11 +5,11 @@ import { useForm } from '@mantine/form';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
 import { showNotification } from '@mantine/notifications';
-import { createCartedDonation } from '@/utils/createCartedDonation';
 import { CartedDonationData } from '../Interfaces/CartedDonationData';
 
 export default function AddToCartComponent() {
   const { data: session } = useSession();
+
   const form = useForm({
     initialValues: {
       donation_id: 3,
@@ -17,11 +17,26 @@ export default function AddToCartComponent() {
     },
   });
 
-   // Form Submission Method
-   const handleSubmit = async (values: CartedDonationData) => {
+  // Form Submission Method
+  const handleSubmit = async (values: CartedDonationData) => {
     try {
       if (session) {
-        await createCartedDonation(values);
+        const token = session.access_token;
+        const response = await fetch('http://localhost:8080/carts/add_to_cart/', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(values),
+        });
+
+        if (!response.ok) {
+          throw new Error('Failed to add item to cart');
+        }
+
+        await response.json(); // will need to add useState here
+
         showNotification({
           title: 'Success',
           color: 'green',
@@ -36,6 +51,7 @@ export default function AddToCartComponent() {
         });
       }
     } catch (error) {
+      console.error('Error adding item to cart:', error);
       showNotification({
         title: 'Error',
         color: 'red',
@@ -48,35 +64,37 @@ export default function AddToCartComponent() {
 
   if (!session) {
     return (
-    <div>
-      <Link href="/signin">You must be signed in to add to a cart.</Link>
-    </div>
+      <div>
+        <Link href="/signin">You must be signed in to add to a cart.</Link>
+      </div>
     );
   }
 
   return (
     <Paper withBorder p="md" radius="md">
       <form onSubmit={form.onSubmit(handleSubmit)}>
-      <Group justify="space-between">
-        <Group align="flex-end" gap="xs">
-          <Text c="grey" fz="sm" fw={700}>
-            <span>
-              <IconShoppingCart />
-            </span>
-          </Text>
+        <Group justify="space-between">
+          <Group align="flex-end" gap="xs">
+            <Text c="grey" fz="sm" fw={700}>
+              <span>
+                <IconShoppingCart />
+              </span>
+            </Text>
+          </Group>
+          <IconDeviceAnalytics size="1.4rem" stroke={1.5} />
         </Group>
-        <IconDeviceAnalytics size="1.4rem" stroke={1.5} />
-      </Group>
-      <h4>Claim Donation</h4>
-      <NumberInput
-        label="Quantity"
-        key={form.values.quantity}
-        placeholder="0"
-        {...form.getInputProps('quantity')}
-      />
-      <Group justify="space-between" mt="xl">
-      <Button type="submit" color="green"> Add to Cart </Button>
-      </Group>
+        <h4>Claim Donation</h4>
+        <NumberInput
+          label="Quantity"
+          key={form.values.quantity}
+          placeholder="0"
+          {...form.getInputProps('quantity')}
+        />
+        <Group justify="space-between" mt="xl">
+          <Button type="submit" color="green">
+            Add to Cart
+          </Button>
+        </Group>
       </form>
     </Paper>
   );
