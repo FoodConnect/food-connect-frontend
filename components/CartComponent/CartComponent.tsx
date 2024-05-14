4cb69b2d82e4f19c738c63fd6f28940b4f816ef8
kyle-pazdel <kyle.kayvaun@gmail.com>
2024-05-13 19:46:30 -0700

    Refactors CartComponent with props and individual state variables for each carted donation object, conditionally renders no-cart Hero Title component

--- components/CartComponent/CartComponent.tsx
+++ components/CartComponent/CartComponent.tsx
@@ -1,1 +1,122 @@
+import { useEffect, useState } from 'react';
+import { Card, Button, Image, NumberInput } from '@mantine/core';
+import { showNotification } from '@mantine/notifications';
+import { useSession } from 'next-auth/react';
+import { CartedDonationData } from '@/components/Interfaces/CartedDonationData';
 
+interface CartProps {
+  cartedDonation: CartedDonationData;
+  userCartId: number;
+  cartedDonations: CartedDonationData[];
+  updateCartedDonations: (updatedDonations: CartedDonationData[]) => void;
+}
+
+const CartComponent = (props: CartProps) => {
+  const { data: session } = useSession();
+  const [quantity, setQuantity] = useState(props.cartedDonation?.quantity ?? 0);
+
+  const handleUpdateQuantity = async () => {
+    try {
+      const token = session?.access_token;
+      const response = await fetch(`http://localhost:8080/carts/${props.userCartId}/update_cart/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          donation_id: props.cartedDonation?.donation?.id,
+          quantity,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to update cart quantity');
+      }
+
+      showNotification({
+        title: 'Success',
+        color: 'green',
+        message: 'Quantity updated!',
+      });
+    } catch (error) {
+      console.error('Error updating quantity:', error);
+
+      showNotification({
+        title: 'Error',
+        message: 'Failed to update quantity',
+        color: 'red',
+      });
+    }
+  };
+
+  const handleDeleteDonation = async () => {
+    try {
+      const token = session?.access_token;
+      await fetch(`http://localhost:8080/carted_donations/${props.cartedDonation?.id}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const updatedDonations = props.cartedDonations.filter(
+        (donation) => donation.id !== props.cartedDonation?.donation?.id
+      );
+      props.updateCartedDonations(updatedDonations);
+      showNotification({
+        title: 'Success',
+        color: 'green',
+        message: 'Quantity updated!',
+      });
+    } catch (error) {
+      console.error('Error deleting donation:', error);
+    }
+  };
+
+  useEffect(() => {}, [session]);
+
+  return (
+    <Card
+      key={props.cartedDonation.id}
+      shadow="xs"
+      padding="md"
+      withBorder
+      style={{ width: '100%' }}
+    >
+      <div style={{ display: 'flex' }}>
+        <div style={{ width: '200px', flexShrink: 0 }}>
+          <Image
+            src={props.cartedDonation.donation?.image_data}
+            alt="Donation Image"
+            fit="contain"
+            width="200px"
+            height="220px"
+          />
+        </div>
+        <div style={{ flex: 1, paddingLeft: '20px' }}>
+          <h3>{props.cartedDonation.donation?.description}</h3>
+          <p>{props.cartedDonation.donation?.donor?.business_name}</p>
+
+          <div style={{ width: '150px', paddingBottom: '10px' }}>
+            <p>Quantity</p>
+            <NumberInput
+              label="Quantity"
+              value={quantity}
+              clampBehavior="strict"
+              min={0}
+              max={props.cartedDonation.donation?.remaining_inventory}
+              onChange={(value) => setQuantity(Number(value))}
+            />
+          </div>
+
+          <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
+            <Button onClick={() => handleUpdateQuantity()}>Update Donation</Button>
+            <Button onClick={() => handleDeleteDonation()}>Delete Donation</Button>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+};
+export default CartComponent;
--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,16 +1,16 @@
-import { Grid, Container, Card, Button, Image, NumberInput } from '@mantine/core';
-import { showNotification } from '@mantine/notifications';
+import { Grid, Container, Card } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import OrderSummaryComponent from '@/components/OrderSummaryComponent/OrderSummaryComponent';
 import { CartData } from '@/components/Interfaces/CartData';
 import { CartedDonationData } from '@/components/Interfaces/CartedDonationData';
+import CartComponent from '@/components/CartComponent/CartComponent';
+import { NoCartHeroTitle } from '@/components/HeroTitle/NoCartHeroTitle';
 
 export default function CartPage() {
   const { data: session } = useSession();
   const [userCart, setUserCart] = useState<CartData>();
   const [cartedDonations, setCartedDonations] = useState<CartedDonationData[]>([]);
-  const [newQuantities, setNewQuantities] = useState<{ [key: string]: number }>({});
 
   async function fetchData() {
     if (!session) {
@@ -32,6 +32,10 @@ export default function CartPage() {
     return response.json();
   }
 
+  const updateCartedDonations = (updatedDonations: CartedDonationData[]) => {
+    setCartedDonations(updatedDonations);
+  };
+
   useEffect(() => {
     if (!session) return;
 
@@ -44,134 +48,33 @@ export default function CartPage() {
         console.error('Error fetching cart data:', error);
       }
     }
-
     getData();
   }, [session]);
 
- // Function to handle quantity update
- const handleUpdateQuantity = async (donationId: number) => {
-  console.log('DONATION ID: ', donationId);
-  try {
-    const token = session?.access_token;
-    const response = await fetch(`http://localhost:8080/carts/${userCart?.id}/update_cart/`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify({
-        donation_id: donationId,
-        quantity: newQuantities[String(donationId)],
-      }),
-    });
-
-    console.log(JSON.stringify({
-      donation_id: donationId,
-      quantity: newQuantities[String(donationId)],
-    }));
-
-    if (!response.ok) {
-      throw new Error('Failed to update cart quantity');
-    }
-
-    showNotification({
-      title: 'Success',
-      color: 'green',
-      message: 'Quantity updated!',
-    });
-  } catch (error) {
-    console.error('Error updating quantity:', error);
-
-    showNotification({
-      title: 'Error',
-      message: 'Failed to update quantity',
-      color: 'red',
-    });
-  }
-};
-
-  // Function to handle quantity change in NumberInput
-  const handleQuantityChange = (value: string | number, donationId: number) => {
-    setNewQuantities((prevQuantities) => ({
-      ...prevQuantities,
-      [String(donationId)]: typeof value === 'string' ? parseInt(value, 10) : value,
-    }));
-  };
-
-  const handleDeleteDonation = async (donationId: any) => {
-    try {
-      const token = session?.access_token;
-      await fetch(`http://localhost:8080/carted_donations/${donationId}/`, {
-        method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      const updatedDonations = cartedDonations.filter(
-        (donation) => donation.id !== donationId
-      );
-
-      setCartedDonations(updatedDonations);
-    } catch (error) {
-      console.error('Error deleting donation:', error);
-    }
-  };
-
-  return (
+  return cartedDonations.length === 0 ? (
+    <NoCartHeroTitle />
+  ) : (
     <Container my="md">
       <h1>Your Cart</h1>
       <Grid>
         <Grid.Col span={{ base: 12, xs: 8 }}>
           <Container my="md">
             <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
-              {cartedDonations.map((donation) => (
-                <Card key={donation.id} shadow="xs" padding="md" withBorder style={{ width: '100%' }}>
-                  <div style={{ display: 'flex' }}>
-                    <div style={{ width: '200px', flexShrink: 0 }}>
-                      <Image
-                        src={donation.donation?.image_data}
-                        alt="Donation Image"
-                        fit="contain"
-                        width="200px"
-                        height="220px"
-                      />
-                    </div>
-                    <div style={{ flex: 1, paddingLeft: '20px' }}>
-                      <h3>{donation.donation?.description}</h3>
-                      <p>{donation.donation?.donor?.business_name}</p>
-
-                        <div style={{ width: '150px', paddingBottom: '10px' }}>
-                          <p>Quantity</p>
-                          <NumberInput
-                            label="Quantity"
-                            value={newQuantities[String(donation.id)] !== undefined ?
-                               newQuantities[String(donation.id)].toString() :
-                                donation.quantity.toString()}
-                            onChange={(value) => handleQuantityChange(value, donation.id!)}
-                          />
-                        </div>
-
-                        <div style={{ display: 'flex', alignItems: 'center', marginBottom: '20px' }}>
-                        <Button onClick={() => handleUpdateQuantity(donation.id!)}>
-                          Update Donation
-                        </Button>
-                          <Button onClick={() => handleDeleteDonation(donation.id!)}>
-                            Delete Donation
-                          </Button>
-                        </div>
-                    </div>
-                  </div>
-                </Card>
+              {cartedDonations.map((cartedDonation) => (
+                <CartComponent
+                  key={cartedDonation.id}
+                  cartedDonations={cartedDonations}
+                  updateCartedDonations={updateCartedDonations}
+                  cartedDonation={cartedDonation}
+                  userCartId={userCart?.id!}
+                />
               ))}
             </div>
           </Container>
         </Grid.Col>
-
         <Grid.Col span={{ base: 12, xs: 4 }}>
           <OrderSummaryComponent isCartPage />
         </Grid.Col>
-
         <Grid.Col span={{ base: 12, xs: 12 }}>
           <Card h={140} radius="md" bg="teal.2" />
         </Grid.Col>
