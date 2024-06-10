36d8c7a5c3f6ae7b442e8f8892a679bd71028bf6
Leah Perri <perrileah2023@gmail.com>
2024-06-10 11:53:28 -0500

    geocoding function for address to lat/long

---
+++ components/MapComponent/geocode.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+const GEOCODE_API_URL = 'https://api.opencagedata.com/geocode/v1/json';
+const API_KEY = process.env.NEXT_OPENCAGE_API_KEY;
+
+interface GeocodeResponse {
+  lat: number;
+  lng: number;
+}
+
+export const geocodeAddress = async (address: string): Promise<GeocodeResponse> => {
+  const response = await axios.get(GEOCODE_API_URL, {
+    params: {
+      key: API_KEY,
+      q: address,
+      limit: 1,
+    },
+  });
+
+  if (response.data.results.length > 0) {
+    const { lat, lng } = response.data.results[0].geometry;
+    return { lat, lng };
+  }
+    throw new Error('Geocoding failed');
+};
